2c19808124cfb15ee509e0e221f0f109172b8483
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-02-01 20:11:35 -0300

    Added annotation EnableCassandraAuditing

--- src/main/java/com/chat/api/ApiApplication.java
+++ src/main/java/com/chat/api/ApiApplication.java
@@ -2,8 +2,10 @@ package com.chat.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.cassandra.config.EnableCassandraAuditing;
 
 @SpringBootApplication
+@EnableCassandraAuditing
 public class ApiApplication {
 
 	public static void main(String[] args) {
