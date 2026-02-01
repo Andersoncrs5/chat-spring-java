c84227a5605a51de5427d70a5366e5349387e962
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-02-01 20:30:09 -0300

    Added annotation EnableConfigurationProperties

--- src/main/java/com/chat/api/ApiApplication.java
+++ src/main/java/com/chat/api/ApiApplication.java
@@ -1,11 +1,14 @@
 package com.chat.api;
 
+import com.chat.api.configs.parameters.JwtParameter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.cassandra.config.EnableCassandraAuditing;
 
 @SpringBootApplication
 @EnableCassandraAuditing
+@EnableConfigurationProperties(JwtParameter.class)
 public class ApiApplication {
 
 	public static void main(String[] args) {
