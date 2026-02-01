ad0a22bd5fd5e722ae6f2f6c86052a6edd0a87c1
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-02-01 20:11:10 -0300

    New table user

---
+++ src/main/java/com/chat/api/models/UserModel.java
@@ -0,0 +1,45 @@
+package com.chat.api.models;
+
+import com.chat.api.utils.base.models.BaseModel;
+import org.springframework.data.cassandra.core.mapping.Column;
+import org.springframework.data.cassandra.core.mapping.Table;
+import org.springframework.data.cassandra.core.mapping.Indexed;
+import java.time.OffsetDateTime;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Table("users")
+@Getter
+@Setter
+@ToString(callSuper = true)
+@SuperBuilder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserModel extends BaseModel {
+
+    @Column("name")
+    private String name;
+
+    @Indexed
+    @Column("username")
+    private String username;
+
+    @Column("banner_url")
+    private String bannerUrl;
+
+    @Indexed
+    @Column("email")
+    private String email;
+
+    @Column("password")
+    private String password;
+
+    @Column("refresh_token")
+    private String refreshToken;
+
+    @Column("phone_number")
+    private String phoneNumber;
+
+    @Column("login_block_at")
+    private OffsetDateTime loginBlockAt;
+}
