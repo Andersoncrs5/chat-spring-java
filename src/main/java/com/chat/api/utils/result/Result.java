5587261f68e71885f6ac0bdad56252894e0a9ed8
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-01 13:13:14 -0300

    Improvement and add column status

--- src/main/java/com/chat/api/utils/result/Result.java
+++ src/main/java/com/chat/api/utils/result/Result.java
@@ -1,7 +1,8 @@
 package com.chat.api.utils.result;
 
 import lombok.Getter;
-import java.util.ArrayList;
+import org.springframework.http.HttpStatus;
+
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Consumer;
@@ -13,50 +14,60 @@ public class Result<T> {
     private final T value;
     private final boolean isSuccess;
     private final List<String> errors;
+    private final HttpStatus status;
 
-    private Result(T value, boolean isSuccess, List<String> errors) {
+    private Result(T value, boolean isSuccess, List<String> errors, HttpStatus status) {
         this.value = value;
         this.isSuccess = isSuccess;
-        this.errors = Collections.unmodifiableList(errors);
+        this.errors = errors != null ? Collections.unmodifiableList(errors) : Collections.emptyList();
+        this.status = status;
     }
 
     public static <T> Result<T> success(T value) {
-        return new Result<>(value, true, new ArrayList<>());
+        return new Result<>(value, true, null, HttpStatus.OK);
     }
 
-    public static <T> Result<T> failure(String error) {
-        return new Result<>(null, false, List.of(error));
+    public static <T> Result<T> created(T value) {
+        return new Result<>(value, true, null, HttpStatus.CREATED);
     }
 
-    public static <T> Result<T> failure(List<String> errors) {
-        return new Result<>(null, false, errors);
+    public static <T> Result<T> failure(String error, HttpStatus status) {
+        return new Result<>(null, false, List.of(error), status);
+    }
+
+    public static <T> Result<T> failure(List<String> errors, HttpStatus status) {
+        return new Result<>(null, false, errors, status);
     }
 
     public boolean isFailure() {
         return !isSuccess;
     }
+
+    public int getStatusCode() {
+        return status.value();
+    }
+
     public <U> Result<U> map(Function<T, U> mapper) {
         if (isSuccess) {
-            return Result.success(mapper.apply(value));
+            return new Result<>(mapper.apply(value), true, null, this.status);
         }
-        return Result.failure(this.errors);
+        return Result.failure(this.errors, this.status);
     }
 
     public Result<T> ifSuccess(Consumer<T> action) {
-        if (isSuccess) {
-            action.accept(value);
-        }
+        if (isSuccess) action.accept(value);
+
         return this;
     }
 
     public Result<T> ifFailure(Consumer<List<String>> action) {
-        if (isFailure()) {
-            action.accept(errors);
-        }
+        if (isFailure()) action.accept(errors);
+
         return this;
     }
 
     public T orElse(T defaultValue) {
         return isSuccess ? value : defaultValue;
     }
+
 }
