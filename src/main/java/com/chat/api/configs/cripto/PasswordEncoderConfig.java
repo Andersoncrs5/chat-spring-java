47735c61628f71ec665c1a6eaf2ebc51eff18e41
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-01 15:50:15 -0300

    Configured PasswordEncoderConfig

---
+++ src/main/java/com/chat/api/configs/cripto/PasswordEncoderConfig.java
@@ -0,0 +1,25 @@
+package com.chat.api.configs.cripto;
+
+import com.chat.api.configs.parameters.ArgonProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableConfigurationProperties(ArgonProperties.class)
+public class PasswordEncoderConfig {
+
+    @Bean
+    public Argon2PasswordEncoder passwordEncoder(ArgonProperties props) {
+        return new Argon2PasswordEncoder(
+                props.saltLength(),
+                props.hashLength(),
+                props.parallelism(),
+                props.memory(),
+                props.iterations()
+        );
+    }
+}
