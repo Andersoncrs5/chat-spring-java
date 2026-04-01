df53b4ce0ac54e6ec75838816d298547325e0bca
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-01 15:56:38 -0300

    Set up UserMapper

---
+++ src/main/java/com/chat/api/utils/mapper/user/UserMapper.java
@@ -0,0 +1,23 @@
+package com.chat.api.utils.mapper.user;
+
+import com.chat.api.configs.mapstruct.DateMapper;
+import com.chat.api.modules.user.dto.CreateUserDTO;
+import com.chat.api.modules.user.dto.UpdateUserDTO;
+import com.chat.api.modules.user.dto.UserDTO;
+import com.chat.api.modules.user.model.UserModel;
+import org.mapstruct.*;
+
+@Mapper(
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        uses = { DateMapper.class }
+)
+public interface UserMapper {
+
+    UserDTO toDto(UserModel model);
+
+    UserModel toModel(CreateUserDTO dto);
+
+    void updateModelFromDto(UpdateUserDTO dto, @MappingTarget UserModel model);
+}
