eee33f7f293f88665e1fa3a306eeb4ced5939281
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-01 15:18:53 -0300

    Set up JacksonConfig

---
+++ src/main/java/com/chat/api/configs/jackson/JacksonConfig.java
@@ -0,0 +1,22 @@
+package com.chat.api.configs.jackson;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+
+        mapper.registerModule(new JavaTimeModule());
+        mapper.registerModule(new ParameterNamesModule());
+        mapper.registerModule(new Jdk8Module());
+        return mapper;
+    }
+}
