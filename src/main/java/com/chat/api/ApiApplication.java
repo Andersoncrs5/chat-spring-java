180d104c1785fb74f2fa991be3516a7c5c470357
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-01 15:20:05 -0300

    Added more parameters into EnableConfigurationProperties

--- src/main/java/com/chat/api/ApiApplication.java
+++ src/main/java/com/chat/api/ApiApplication.java
@@ -1,14 +1,16 @@
 package com.chat.api;
 
+import com.chat.api.configs.parameters.FrontParameter;
 import com.chat.api.configs.parameters.JwtParameter;
+import com.chat.api.configs.parameters.RoleParameter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.data.cassandra.config.EnableCassandraAuditing;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
-@EnableCassandraAuditing
-@EnableConfigurationProperties(JwtParameter.class)
+@EnableMongoAuditing
+@EnableConfigurationProperties({JwtParameter.class, FrontParameter.class, RoleParameter.class})
 public class ApiApplication {
 
 	public static void main(String[] args) {
