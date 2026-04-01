5fc07c33f74849418447f0594815219673415f75
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-01 13:06:11 -0300

    Set up UserPrincipal

---
+++ src/main/java/com/chat/api/configs/security/UserPrincipal.java
@@ -0,0 +1,26 @@
+package com.chat.api.configs.security;
+
+import com.chat.api.modules.user.model.UserModel;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+import java.util.UUID;
+
+public class UserPrincipal extends User {
+    private final UUID id;
+    private final UserModel user;
+
+    public UserPrincipal(
+            UUID id,
+            String username,
+            @Nullable String password,
+            Collection<? extends GrantedAuthority> authorities,
+            UserModel user
+    ) {
+        super(username, password, authorities);
+        this.id = id;
+        this.user = user;
+    }
+}
