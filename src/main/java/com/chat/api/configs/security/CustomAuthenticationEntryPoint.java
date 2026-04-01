7849a4f5c2b156184b9183562a61efdd0ef0427e
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-01 13:05:15 -0300

    Set up CustomAuthenticationEntryPoint

---
+++ src/main/java/com/chat/api/configs/security/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,41 @@
+package com.chat.api.configs.security;
+
+import com.chat.api.utils.res.ResponseHttp;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void commence(@NonNull HttpServletRequest request, HttpServletResponse response, @NonNull AuthenticationException authException) throws IOException {
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        var body = new ResponseHttp<Void>(
+                null,
+                "Login invalid",
+                UUID.randomUUID().toString(),
+                1,
+                true,
+                OffsetDateTime.now()
+        );
+
+        objectMapper.writeValue(response.getOutputStream(), body);
+    }
+}
