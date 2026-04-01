c8df5915bad0f953447214c338e9ffe92c204a50
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-01 13:05:56 -0300

    Impled UserRepository

---
+++ src/main/java/com/chat/api/modules/user/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.chat.api.modules.user.repository;
+
+import com.chat.api.modules.user.model.UserModel;
+import com.chat.api.utils.annotation.global.emailConstraint.EmailConstraint;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends MongoRepository<UserModel, UUID> {
+    Optional<UserModel> findByEmailIgnoreCase(@EmailConstraint String email);
+    boolean existsByEmailIgnoreCase(@EmailConstraint String email);
+}
