846e712771c3d00466bbe073652b8823b4a42d08
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-01 13:06:21 -0300

    New methods

--- src/main/java/com/chat/api/modules/user/model/UserModel.java
+++ src/main/java/com/chat/api/modules/user/model/UserModel.java
@@ -1,6 +1,7 @@
 package com.chat.api.modules.user.model;
 
 import com.chat.api.utils.base.models.BaseModel;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 import org.springframework.data.mongodb.core.index.Indexed;
@@ -8,14 +9,15 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.time.OffsetDateTime;
+import java.util.HashSet;
 import java.util.Set;
 
 @Document(collection = "users")
 @Getter
-@Setter
+@Setter(AccessLevel.PROTECTED)
 @ToString(callSuper = true)
 @SuperBuilder(toBuilder = true)
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class UserModel extends BaseModel {
 
@@ -45,6 +47,40 @@ public class UserModel extends BaseModel {
     @Field("login_block_at")
     private OffsetDateTime loginBlockAt;
 
+    @Field("attempts_login")
+    private int attemptsLogin = 0;
+
+    @Builder.Default
     @Field("roles")
-    private Set<String> roles;
+    private Set<String> roles = new HashSet<>();
+
+    public void addRole(@NotBlank String role) {
+        this.roles.add(role.toUpperCase());
+    }
+
+    public void removeRole(@NotBlank String role) {
+        this.roles.remove(role.toUpperCase());
+    }
+
+    public void updatePassword(@NotBlank String newEncodedPassword) {
+        this.password = newEncodedPassword;
+    }
+
+    public boolean isLoginBlocked() {
+        if (loginBlockAt == null) return false;
+        return loginBlockAt.isAfter(OffsetDateTime.now());
+    }
+
+    public void recordFailedLogin() {
+        this.attemptsLogin++;
+        if (this.attemptsLogin >= 5) {
+            this.loginBlockAt = OffsetDateTime.now().plusMinutes(15);
+        }
+    }
+
+    public void resetLoginAttempts() {
+        this.attemptsLogin = 0;
+        this.loginBlockAt = null;
+    }
+
 }
