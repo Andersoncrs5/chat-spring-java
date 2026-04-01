be4950da3d21772578d96351c225591dd018111f
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-01 15:49:12 -0300

    Set up CreateUserDTO

---
+++ src/main/java/com/chat/api/modules/user/dto/CreateUserDTO.java
@@ -0,0 +1,11 @@
+package com.chat.api.modules.user.dto;
+
+public record CreateUserDTO(
+        String name,
+        String username,
+        String email,
+        String password,
+        String bannerUrl,
+        String phoneNumber
+) {
+}
