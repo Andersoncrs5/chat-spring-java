e436591525117e85e848554a3ef018821a246e4a
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-05 16:40:38 -0300

    Added MapStructConfig

---
+++ src/main/java/com/chat/api/configs/mapstruct/MapStructConfig.java
@@ -0,0 +1,15 @@
+package com.chat.api.configs.mapstruct;
+
+import org.mapstruct.CollectionMappingStrategy;
+import org.mapstruct.MapperConfig;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+
+@MapperConfig(componentModel = "spring",
+        uses = { DateMapper.class },
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        collectionMappingStrategy = CollectionMappingStrategy.ADDER_PREFERRED
+)
+public class MapStructConfig {
+}
