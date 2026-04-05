5433cbac9c3aeb7d0fa4a8e6877ac20e46303331
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-05 17:53:20 -0300

    Set up AuthModuleGateway

---
+++ src/main/java/com/chat/api/modules/auth/gateway/AuthModuleGateway.java
@@ -0,0 +1,29 @@
+package com.chat.api.modules.auth.gateway;
+
+import com.chat.api.modules.user.model.UserModel;
+import com.chat.api.modules.user.services.interfaces.IUserService;
+import com.chat.api.utils.result.Result;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class AuthModuleGateway {
+
+    private final IUserService userService;
+
+    public Result<UserModel> findUserByEmail(String email) {
+        return this.userService.findByEmail(email);
+    }
+
+    public Result<UserModel> blockUser(UUID id) {
+        return this.userService.blockUser(id);
+    }
+
+    public Result<UserModel> setLastLogin(UUID id) {
+        return this.userService.setLastLogin(id);
+    }
+
+}
