c9b26e0bdacc33898c450636f605210298592a43
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-05 17:58:22 -0300

    Configured pool and another thing

--- src/main/java/com/chat/api/configs/mongo/MongoConfig.java
+++ src/main/java/com/chat/api/configs/mongo/MongoConfig.java
@@ -1,11 +1,21 @@
 package com.chat.api.configs.mongo;
 
+import com.chat.api.configs.times.OffsetDateTimeReadConverter;
+import com.chat.api.configs.times.OffsetDateTimeWriteConverter;
 import org.bson.UuidRepresentation;
 import org.springframework.boot.mongodb.autoconfigure.MongoClientSettingsBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import java.time.OffsetDateTime;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class MongoConfig {
@@ -18,6 +28,29 @@ public class MongoConfig {
     @Bean
     MongoTransactionManager transactionManager(MongoDatabaseFactory dbFactory) {
         return new MongoTransactionManager(dbFactory);
+    }
+
+    @Bean
+    public MongoCustomConversions customConversions() {
+        return new MongoCustomConversions(List.of(
+                new OffsetDateTimeReadConverter(),
+                new OffsetDateTimeWriteConverter()
+        ));
+    }
+
+    @Bean
+    public ValidatingMongoEventListener validatingMongoEventListener(
+            LocalValidatorFactoryBean factory) {
+        return new ValidatingMongoEventListener(factory);
+    }
+
+    @Bean
+    public MongoClientSettingsBuilderCustomizer connectionPoolCustomizer() {
+        return builder -> builder.applyToConnectionPoolSettings(settings ->
+                settings.maxSize(50)
+                        .minSize(20)
+                        .maxWaitTime(2000, TimeUnit.MILLISECONDS)
+        );
     }
 
 }
