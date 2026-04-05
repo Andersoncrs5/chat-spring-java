854ef4871e8455ffcfa814cc14942bca9bd98bdc
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-05 16:40:47 -0300

    Added methods

--- src/main/java/com/chat/api/modules/user/services/interfaces/IUserService.java
+++ src/main/java/com/chat/api/modules/user/services/interfaces/IUserService.java
@@ -1,4 +1,14 @@
 package com.chat.api.modules.user.services.interfaces;
 
+import com.chat.api.modules.user.dto.CreateUserDTO;
+import com.chat.api.modules.user.model.UserModel;
+import com.chat.api.utils.annotation.global.isModelInitialized.IsModelInitialized;
+import com.chat.api.utils.result.Result;
+
+import java.util.UUID;
+
 public interface IUserService {
+    Result<UserModel> create(CreateUserDTO dto);
+    void delete(@IsModelInitialized UserModel user);
+    Result<UserModel> findById(UUID id);
 }
