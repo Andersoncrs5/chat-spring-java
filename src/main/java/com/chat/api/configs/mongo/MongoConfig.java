96e263848b03239b9ec329caf8084533bb813f31
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-05 17:54:05 -0300

    Set up MongoTransactionManager

--- src/main/java/com/chat/api/configs/mongo/MongoConfig.java
+++ src/main/java/com/chat/api/configs/mongo/MongoConfig.java
@@ -4,6 +4,8 @@ import org.bson.UuidRepresentation;
 import org.springframework.boot.mongodb.autoconfigure.MongoClientSettingsBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
 
 @Configuration
 public class MongoConfig {
@@ -12,4 +14,10 @@ public class MongoConfig {
     public MongoClientSettingsBuilderCustomizer uuidCustomizer() {
         return builder -> builder.uuidRepresentation(UuidRepresentation.STANDARD);
     }
+
+    @Bean
+    MongoTransactionManager transactionManager(MongoDatabaseFactory dbFactory) {
+        return new MongoTransactionManager(dbFactory);
+    }
+
 }
