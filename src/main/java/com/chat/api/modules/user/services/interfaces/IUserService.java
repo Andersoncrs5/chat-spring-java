6b550313984093a43fec973ef745c0f4d53e7a1d
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-06 17:32:22 -0300

    Added any methods

--- src/main/java/com/chat/api/modules/user/services/interfaces/IUserService.java
+++ src/main/java/com/chat/api/modules/user/services/interfaces/IUserService.java
@@ -4,6 +4,7 @@ import com.chat.api.modules.user.dto.CreateUserDTO;
 import com.chat.api.modules.user.dto.UpdateUserDTO;
 import com.chat.api.modules.user.dto.UserFilterDTO;
 import com.chat.api.modules.user.model.UserModel;
+import com.chat.api.utils.annotation.global.emailConstraint.EmailConstraint;
 import com.chat.api.utils.annotation.global.isModelInitialized.IsModelInitialized;
 import com.chat.api.utils.result.Result;
 import org.springframework.data.domain.Page;
@@ -22,5 +23,12 @@ public interface IUserService {
     Result<UserModel> update(
             @IsModelInitialized UserModel user,
             UpdateUserDTO dto
+    );
+    Result<UserModel> findByEmail(@EmailConstraint String email);
+    Result<UserModel> blockUser(
+            UUID userID
+    );
+    Result<UserModel> setLastLogin(
+            UUID userID
     );
 }
