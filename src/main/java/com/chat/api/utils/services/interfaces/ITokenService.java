6a7c1bf523b8f7d6b9d4437d518f8b0c0a89dd0f
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-06 17:30:31 -0300

    Added method generateResponseToken

--- src/main/java/com/chat/api/utils/services/interfaces/ITokenService.java
+++ src/main/java/com/chat/api/utils/services/interfaces/ITokenService.java
@@ -1,9 +1,13 @@
 package com.chat.api.utils.services.interfaces;
 
 import com.chat.api.modules.user.model.UserModel;
+import com.chat.api.utils.annotation.global.isModelInitialized.IsModelInitialized;
+import com.chat.api.utils.res.ResponseToken;
+import jakarta.validation.constraints.NotBlank;
 
 public interface ITokenService {
     String generateRefreshToken(UserModel user);
     String generateToken(UserModel user);
-    String validateToken(String token);
+    String validateToken(@NotBlank String token);
+    ResponseToken generateResponseToken(@IsModelInitialized UserModel user);
 }
