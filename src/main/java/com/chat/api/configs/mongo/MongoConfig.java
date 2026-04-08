2611727c53c2905ce2a35028b6c88e8d2135c3df
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-08 14:08:27 -0300

    Removed transactionManager and customConversions

--- src/main/java/com/chat/api/configs/mongo/MongoConfig.java
+++ src/main/java/com/chat/api/configs/mongo/MongoConfig.java
@@ -1,20 +1,12 @@
 package com.chat.api.configs.mongo;
 
-import com.chat.api.configs.times.OffsetDateTimeReadConverter;
-import com.chat.api.configs.times.OffsetDateTimeWriteConverter;
 import org.bson.UuidRepresentation;
 import org.springframework.boot.mongodb.autoconfigure.MongoClientSettingsBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.MongoDatabaseFactory;
-import org.springframework.data.mongodb.MongoTransactionManager;
-import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
 import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
-import java.time.OffsetDateTime;
-import java.util.Date;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
@@ -24,19 +16,6 @@ public class MongoConfig {
     public MongoClientSettingsBuilderCustomizer uuidCustomizer() {
         return builder -> builder.uuidRepresentation(UuidRepresentation.STANDARD);
     }
-
-    @Bean
-    MongoTransactionManager transactionManager(MongoDatabaseFactory dbFactory) {
-        return new MongoTransactionManager(dbFactory);
-    }
-
-//    @Bean
-//    public MongoCustomConversions customConversions() {
-//        return new MongoCustomConversions(List.of(
-//                new OffsetDateTimeReadConverter(),
-//                new OffsetDateTimeWriteConverter()
-//        ));
-//    }
 
     @Bean
     public ValidatingMongoEventListener validatingMongoEventListener(
