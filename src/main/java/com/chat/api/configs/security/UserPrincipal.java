518c07e3c031d6cf1e4880189c403f15f4e6fc0f
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-08 14:08:47 -0300

    Added @Getter

--- src/main/java/com/chat/api/configs/security/UserPrincipal.java
+++ src/main/java/com/chat/api/configs/security/UserPrincipal.java
@@ -1,6 +1,7 @@
 package com.chat.api.configs.security;
 
 import com.chat.api.modules.user.model.UserModel;
+import lombok.Getter;
 import org.jspecify.annotations.Nullable;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -8,6 +9,7 @@ import org.springframework.security.core.userdetails.User;
 import java.util.Collection;
 import java.util.UUID;
 
+@Getter
 public class UserPrincipal extends User {
     private final UUID id;
     private final UserModel user;
