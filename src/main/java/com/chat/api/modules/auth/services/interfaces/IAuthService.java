ab4209e71c1039f10b30d42dddd776a487e2bc17
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-08 09:46:18 -0300

    New method refreshToken

--- src/main/java/com/chat/api/modules/auth/services/interfaces/IAuthService.java
+++ src/main/java/com/chat/api/modules/auth/services/interfaces/IAuthService.java
@@ -4,8 +4,10 @@ import com.chat.api.modules.auth.dto.LoginRequestDTO;
 import com.chat.api.modules.user.dto.CreateUserDTO;
 import com.chat.api.utils.res.ResponseToken;
 import com.chat.api.utils.result.Result;
+import jakarta.validation.constraints.NotBlank;
 
 public interface IAuthService {
     Result<ResponseToken> login(LoginRequestDTO dto);
     Result<ResponseToken> create(CreateUserDTO dto);
+    Result<ResponseToken> refreshToken(@NotBlank String refreshToken);
 }
