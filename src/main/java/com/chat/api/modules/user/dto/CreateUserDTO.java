8f42780692d892777fbd6597ec781732e08e5b53
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-08 14:08:11 -0300

    Added validation

--- src/main/java/com/chat/api/modules/user/dto/CreateUserDTO.java
+++ src/main/java/com/chat/api/modules/user/dto/CreateUserDTO.java
@@ -1,9 +1,12 @@
 package com.chat.api.modules.user.dto;
 
+import jakarta.validation.constraints.Size;
+
 public record CreateUserDTO(
         String name,
         String username,
         String email,
+        @Size(min = 8, max = 50)
         String password,
         String bannerUrl,
         String bio,
