c037f93519995a948f86364e7db7decaebdb463a
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-07 21:24:32 -0300

    New method endpoints create and login

--- src/main/java/com/chat/api/modules/auth/controller/docs/IAuthControllerDocs.java
+++ src/main/java/com/chat/api/modules/auth/controller/docs/IAuthControllerDocs.java
@@ -1,4 +1,22 @@
 package com.chat.api.modules.auth.controller.docs;
 
+import com.chat.api.modules.auth.dto.LoginRequestDTO;
+import com.chat.api.modules.user.dto.CreateUserDTO;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 public interface IAuthControllerDocs {
+    @PostMapping("/register")
+    ResponseEntity<?> create(
+            @Valid @RequestBody CreateUserDTO dto,
+            HttpServletRequest request
+    );
+    @PostMapping("/login")
+    ResponseEntity<?> login(
+            @Valid @RequestBody LoginRequestDTO dto,
+            HttpServletRequest request
+    );
 }
--- src/main/java/com/chat/api/modules/auth/controller/provider/AuthController.java
+++ src/main/java/com/chat/api/modules/auth/controller/provider/AuthController.java
@@ -1,10 +1,19 @@
 package com.chat.api.modules.auth.controller.provider;
 
 import com.chat.api.modules.auth.controller.docs.IAuthControllerDocs;
-import com.chat.api.modules.user.services.interfaces.IUserService;
-import com.chat.api.utils.services.interfaces.ITokenService;
+import com.chat.api.modules.auth.dto.LoginRequestDTO;
+import com.chat.api.modules.auth.services.interfaces.IAuthService;
+import com.chat.api.modules.user.dto.CreateUserDTO;
+import com.chat.api.utils.mapper.user.UserMapper;
+import com.chat.api.utils.res.ResponseHttp;
+import com.chat.api.utils.res.ResponseToken;
+import com.chat.api.utils.result.Result;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +23,42 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/auth/")
 public class AuthController implements IAuthControllerDocs {
 
-    private final IUserService service;
-    private final ITokenService tokenService;
+    private final IAuthService service;
+    private final UserMapper mapper;
 
+    @Override
+    public ResponseEntity<?> login(
+            @Valid @RequestBody LoginRequestDTO dto,
+            HttpServletRequest request
+    ) {
+        Result<ResponseToken> tokenResult = this.service.login(dto);
 
+        if (tokenResult.isFailure()) {
+            return ResponseEntity
+                    .status(tokenResult.getStatus())
+                    .body(ResponseHttp.error("Error the to log user", tokenResult.getErrors()));
+        }
+
+        return ResponseEntity
+                .ok(ResponseHttp.success(tokenResult.getValue(), "User logged"));
+    }
+
+    @Override
+    public ResponseEntity<?> create(
+            @Valid @RequestBody CreateUserDTO dto,
+            HttpServletRequest request
+    ) {
+        Result<ResponseToken> tokenResult = this.service.create(dto);
+
+        if (tokenResult.isFailure()) {
+            return ResponseEntity
+                    .status(tokenResult.getStatus())
+                    .body(ResponseHttp.error("Error the create user", tokenResult.getErrors()));
+        }
+
+        return ResponseEntity
+                .status(201)
+                .body(ResponseHttp.success(tokenResult.getValue(), "User created"));
+    }
 
 }
