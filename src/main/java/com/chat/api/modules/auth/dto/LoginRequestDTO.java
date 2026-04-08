693dcbf0ce863efe54524495d246f86e649aee2d
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-07 21:25:13 -0300

    Added validation

--- src/main/java/com/chat/api/modules/auth/dto/LoginRequestDTO.java
+++ src/main/java/com/chat/api/modules/auth/dto/LoginRequestDTO.java
@@ -1,10 +1,12 @@
 package com.chat.api.modules.auth.dto;
 
 import com.chat.api.utils.annotation.global.emailConstraint.EmailConstraint;
+import jakarta.validation.constraints.Size;
 
 public record LoginRequestDTO(
         @EmailConstraint
         String email,
+        @Size(min = 8, max = 50)
         String password
 ) {
 }
