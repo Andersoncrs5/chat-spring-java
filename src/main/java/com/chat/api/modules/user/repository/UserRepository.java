b052f8c760e6486dcf0583ee45b1e4e3ac653c9b
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-07 21:24:49 -0300

    New method findByRefreshToken

--- src/main/java/com/chat/api/modules/user/repository/UserRepository.java
+++ src/main/java/com/chat/api/modules/user/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.chat.api.modules.user.repository;
 import com.chat.api.modules.user.custom.repository.CustomUserRepository;
 import com.chat.api.modules.user.model.UserModel;
 import com.chat.api.utils.annotation.global.emailConstraint.EmailConstraint;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Optional;
@@ -12,4 +13,6 @@ public interface UserRepository extends MongoRepository<UserModel, UUID>, Custom
     Optional<UserModel> findByEmailIgnoreCase(@EmailConstraint String email);
     boolean existsByEmailIgnoreCase(@EmailConstraint String email);
     boolean existsByUsernameIgnoreCase(String username);
+
+    Optional<UserModel> findByRefreshToken(@NotBlank String refresh);
 }
