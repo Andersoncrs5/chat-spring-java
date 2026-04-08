9351e90c6f467bd8d4d4c8836207838080df1e75
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-08 09:46:33 -0300

    New method create1

--- src/main/java/com/chat/api/modules/role/services/provider/RoleService.java
+++ src/main/java/com/chat/api/modules/role/services/provider/RoleService.java
@@ -5,7 +5,6 @@ import com.chat.api.modules.role.repository.RoleRepository;
 import com.chat.api.modules.role.services.interfaces.IRoleService;
 import com.chat.api.utils.exceptions.InternalServerErrorException;
 import com.chat.api.utils.result.Result;
-import com.mongodb.DuplicateKeyException;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -27,7 +26,22 @@ public class RoleService implements IRoleService {
 
         try {
             return Result.created(repository.save(role));
-        } catch (DuplicateKeyException e) {
+        } catch (com.mongodb.DuplicateKeyException e) {
+            return Result.failure("Role name: " + name + " already exists", HttpStatus.CONFLICT);
+        } catch (Exception e) {
+            throw new InternalServerErrorException(e);
+        }
+    }
+
+    public Result<RoleModel> create1(@NotBlank String name) {
+        RoleModel role = new RoleModel().toBuilder()
+                .id(UUID.randomUUID())
+                .name(name.toUpperCase())
+                .build();
+
+        try {
+            return Result.created(repository.save(role));
+        } catch (org.springframework.dao.DuplicateKeyException e) {
             return Result.failure("Role name: " + name + " already exists", HttpStatus.CONFLICT);
         } catch (Exception e) {
             throw new InternalServerErrorException(e);
