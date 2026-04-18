3a2d013004d50f2e32ee952a0b2bf2349cbe3dce
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-18 12:21:31 -0300

    New exception BusinessRuleException

---
+++ src/main/java/com/chat/api/utils/exceptions/BusinessRuleException.java
@@ -0,0 +1,19 @@
+package com.chat.api.utils.exceptions;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class BusinessRuleException extends RuntimeException {
+    private final HttpStatus status;
+
+    public BusinessRuleException(String message) {
+        super(message);
+        this.status = HttpStatus.BAD_REQUEST;
+    }
+
+    public BusinessRuleException(String message, HttpStatus status) {
+        super(message);
+        this.status = status;
+    }
+}
