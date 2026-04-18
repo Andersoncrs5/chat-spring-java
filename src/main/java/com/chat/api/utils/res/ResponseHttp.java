afc882688e636166e10bd131cd78168bf982cfa9
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-18 12:24:57 -0300

    Added method success

--- src/main/java/com/chat/api/utils/res/ResponseHttp.java
+++ src/main/java/com/chat/api/utils/res/ResponseHttp.java
@@ -19,21 +19,35 @@ public record ResponseHttp<T>(
 ) {
         private static final Integer API_VERSION = 1;
 
-        public static <T> ResponseHttp<T> success(T data, String message) {
+        public static <T> ResponseHttp<T> success(T data, @NotBlank String message, @NotBlank String traceId) {
+                return new ResponseHttp<>(
+                        data, message, null,
+                        traceId, API_VERSION, true, OffsetDateTime.now()
+                );
+        }
+
+        public static <T> ResponseHttp<T> success(T data, @NotBlank String message) {
                 return new ResponseHttp<>(
                         data, message, null,
                         UUID.randomUUID().toString(), API_VERSION, true, OffsetDateTime.now()
                 );
         }
 
-        public static <T> ResponseHttp<T> error(String message, List<String> errors) {
+        public static <T> ResponseHttp<T> error(@NotBlank String message, List<String> errors) {
                 return new ResponseHttp<>(
                         null, message, errors,
                         UUID.randomUUID().toString(), API_VERSION, false, OffsetDateTime.now()
                 );
         }
 
-        public static <T> ResponseHttp<T> error(String message, String singleError) {
+        public static <T> ResponseHttp<T> error(@NotBlank String message, List<String> errors, @NotBlank String traceId) {
+                return new ResponseHttp<>(
+                        null, message, errors,
+                        traceId, API_VERSION, false, OffsetDateTime.now()
+                );
+        }
+
+        public static <T> ResponseHttp<T> error(@NotBlank String message, String singleError) {
                 return error(message, List.of(singleError));
         }
 }
