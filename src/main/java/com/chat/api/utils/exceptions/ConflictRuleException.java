d7742b14391d5d44ba62279912e30180c45bb6de
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-04-18 12:21:37 -0300

    New exception ConflictRuleException

---
+++ src/main/java/com/chat/api/utils/exceptions/ConflictRuleException.java
@@ -0,0 +1,7 @@
+package com.chat.api.utils.exceptions;
+
+public class ConflictRuleException extends RuntimeException {
+    public ConflictRuleException(String message) {
+        super(message);
+    }
+}
